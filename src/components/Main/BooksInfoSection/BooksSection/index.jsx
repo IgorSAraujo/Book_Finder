5ab7cf48dc6d0309e747ea19b475a425c058b28c
Igor Silva <firetuskbr1@gmail.com>
2024-02-09 23:05:26 -0300

    Funções para renderização dimanicas Criadas

--- src/components/Main/BooksInfoSection/BooksSection/index.jsx
+++ src/components/Main/BooksInfoSection/BooksSection/index.jsx
@@ -1,5 +1,41 @@
 import styles from "./styles.module.scss"
 
-export const BooksSection = () => {
-  return <section></section>
+export const BooksSection = ({ search, bookList }) => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.containerTitle}>
+        <span className="titleH2">Livros listados: {bookList.length}</span>
+      </div>
+      <div className={styles.containerBookList}>
+        {search ? (
+          <p className="textP">
+            Resultados para a busca para:<strong>{search}</strong>
+          </p>
+        ) : null}
+
+        {bookList.length > 0 ? (
+          <ul>
+            {bookList.map((book) => (
+              <li className={styles.card} key={book.id}>
+                <div className={styles.cardTitle}>
+                  <h2 className="titleH2">{book.name}</h2>
+                  <p className="titleP">{book.category}</p>
+                </div>
+                <div className={styles.cardPrice}>
+                  <span className="titleH2">
+                    {book.price.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="textP">Nenhum Resultado encontrado</p>
+        )}
+      </div>
+    </section>
+  )
 }
--- src/components/Main/BooksInfoSection/index.jsx
+++ src/components/Main/BooksInfoSection/index.jsx
@@ -1,12 +1,49 @@
 import styles from "./styles.module.scss"
+import { useState } from "react"
+import { books } from "../../../data/books.js"
 import { AsideSection } from "./AsideSection"
 import { BooksSection } from "./BooksSection"
 
 export const BooksInfoSection = () => {
+  const [search, setSearch] = useState("")
+  const [category, setCategory] = useState("")
+  const [min, setMin] = useState("")
+  const [max, setMax] = useState("")
+
+  const bookFinder = books.filter((book) => {
+    const searchFilter =
+      search === ""
+        ? true
+        : book.name.toLowerCase().includes(search.toLowerCase()) ||
+          book.category.toLowerCase().includes(search.toLowerCase())
+
+    const categoryFilter = category === "" ? true : book.category === category
+
+    const minPriceFilter = min === "" ? true : book.price >= Number(min)
+    const maxPriceFilter = max === "" ? true : book.price <= Number(max)
+
+    return searchFilter && categoryFilter && minPriceFilter && maxPriceFilter
+  })
+
+  const cleanFilters = () => {
+    setSearch("")
+    setCategory("")
+    setMin("")
+    setMax("")
+  }
+
   return (
     <div className={styles.booksContainer}>
-      <AsideSection />
-      <BooksSection />
+      <AsideSection
+        cleanFilters={cleanFilters}
+        setSearch={setSearch}
+        setCategory={setCategory}
+        min={min}
+        setMin={setMin}
+        max={max}
+        setMax={setMax}
+      />
+      <BooksSection search={search} bookList={bookFinder} />
     </div>
   )
 }
